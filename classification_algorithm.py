c9baea5924bde7c5ec0f082e0ea3abb68b7c2db8
OussamaSassi9 <oussama.sassi@rutilea-tn.com>
2022-06-13 14:59:44 +0000

    yes

--- classification_algorithm.py
+++ classification_algorithm.py
@@ -58,10 +58,8 @@ class ClassificationModel:
 
     def __loadDataSet(self):
         resize_shape = self.input_shape[:2]
-        preprocess = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Resize((224, 224)),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        preprocess = transforms.Compose([ transforms.Resize((224, 224)) ,
+            transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
 
         for i, class_name in enumerate(glob(os.path.join('dataset','*'))):
