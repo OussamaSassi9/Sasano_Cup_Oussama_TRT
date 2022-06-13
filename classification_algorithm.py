a58b90fff3e0bfada3bac850750a70df42dc9a08
OussamaSassi9 <oussama.sassi@rutilea-tn.com>
2022-06-13 15:04:14 +0000

    pil

--- classification_algorithm.py
+++ classification_algorithm.py
@@ -58,7 +58,7 @@ class ClassificationModel:
 
     def __loadDataSet(self):
         resize_shape = self.input_shape[:2]
-        preprocess = transforms.Compose([ transforms.Resize((224, 224)) ,
+        preprocess = transforms.Compose([ transforms.ToPILImage() , transforms.Resize((224, 224)) ,
             transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
 
